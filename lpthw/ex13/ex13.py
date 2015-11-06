984c0f5aa414b4e02065e2af664593d7f740dc04
jsong <archenus@gmail.com>
2015-11-06 16:48:00 +0900

    ex13

--- lpthw/ex13/ex13.py
+++ lpthw/ex13/ex13.py
@@ -1,6 +1,7 @@
 from sys import argv # argv
 
 script, first, second, third = argv # unpacks
+# if I give fewer than three arguments, 
 
 print "The script is called:", script
 print "Your first variable is:", first
